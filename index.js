723277404012d12524f5afec406d625bf4ac7f9c
casper <ycasperinho@gmail.com>
2018-09-30 02:16:05 +0300

    configure coffee-loader

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = function(API) {
+  return API.configureWebpack(function() {
+    return {
+      resolve: {
+        extensions: ['.coffee']
+      },
+      module: {
+        rules: [
+          {
+            test: /\.coffee$/,
+            loader: 'coffee-loader'
+          }
+        ]
+      }
+    };
+  });
+};
