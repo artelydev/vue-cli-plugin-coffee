17d0d9327a25744bb743b27593f98b2aa19d6815
Yaroslav Kasperovich <ykasperovich@conceptant.com>
2020-05-26 19:08:39 +0300

    Resolve node_modules

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-module.exports = (API) => {
+module.exports = (API, projectOptions) => {
   API.chainWebpack((config) => {
+    config.resolveLoader.modules.prepend(path.join(__dirname, "node_modules"));
+
     const coffeeRule = config.module.rule("coffee").test(/\.coffee$/);
 
     coffeeRule.use("cache-loader").loader(require.resolve("cache-loader"));
