666bd645aede47c186cd103ab0fa42c157ddda7a
Yaroslav Kasperovich <ykasperovich@conceptant.com>
2020-05-26 19:12:42 +0300

    Support main.coffee unless config.pages

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ module.exports = (API, projectOptions) => {
   API.chainWebpack((config) => {
     config.resolveLoader.modules.prepend(path.join(__dirname, "node_modules"));
 
+    if (!projectOptions.pages) {
+      config.entry("app").clear().add("./src/main.coffee");
+    }
+
     const coffeeRule = config.module.rule("coffee").test(/\.coffee$/);
 
     coffeeRule.use("cache-loader").loader(require.resolve("cache-loader"));
